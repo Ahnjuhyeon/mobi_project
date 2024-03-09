ac2135c0536afd1a7f4056b1b4c349810cd021a2
sgm4247 <sgm4247@naver.com>
2024-03-09 16:21:15 +0900

    feat >>  필터링 구현 도전중

--- 1week/src/components/filter.jsx
+++ 1week/src/components/filter.jsx
@@ -1,26 +1,34 @@
+import { useEffect } from "react";
 import { FilterTypeSelect } from "../consts/filterType";
+import { useSearchParams } from "react-router-dom";
 
-const FilterPage = ({ searchParams, setSearchParams }) => {
+const FilterPage = ({ userList }) => {
   /*
 1. 50개를 클릭했을때 client.js/userListSlice에 실행되고있는 perPage에 값이 50으로 변경해주기
+=> 문제점 : 다른 value를 눌러도 모두 perPage로 들어간다 
+
 2. searchParams.get으로 perPage 가져와서 .set으로 50으로 바꿔줘보기
 3. setSearchParams으로 상태 변경해보기
-4. 이름순을 클릭하면 
-  */
-  const filter = searchParams.get("filter");
+4. 이름순을 클릭하게되면 
+
+*/
+  const [searchParams, setSearchParams] = useSearchParams();
   //이름 순 정렬 함수
-  const sortByName = (data) => {
-    return data.sort((a, b) => a - b);
-    // 솔트를 해줄거야
+  const sortByName = () => {
+    return userList.sort((a, b) => a.name.localeCompare(b.name));
+    //userDataList.sort((a, b) => a.name.localeCompare(b.name))
+    // 솔트를 해줄거야 아니야..
   };
 
   const onChangeTypeOption = (e) => {
     const value = e.target.value;
-
+    console.log(e.target.name);
+    //위에껀 하다 만거라서 ㅎㅎㅎ..
     setSearchParams((prev) => {
-      prev.set("perPage", value);
+      prev.set(e.target.name, value);
       return prev;
     });
+    //이것때문인것 같은데...
   };
 
   return (
